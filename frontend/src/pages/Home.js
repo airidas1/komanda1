bb44e45ae2528f8580ca1e33125e8641cd8799d1
martynasauglys <martynasauglys@hotmail.com>
2021-04-20 19:25:59 +0300

    test

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 function Home() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <main className='home'>
+      <div className='home-filter'>
+        <form></form>
+      </div>
+      <div className='container'></div>
+    </main>
+  );
 }
 
-export default Home
+export default Home;
