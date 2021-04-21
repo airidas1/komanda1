1b87549113946cbec2da5312c7510efd0887a3cb
martynasauglys <martynasauglys@hotmail.com>
2021-04-21 21:02:29 +0300

    main page almost done, content missing

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Home.css';
+import styles from './Home.module.css';
 import heroImg from '../assets/images/hero-img.png';
 import universityIcon from '../assets/images/university-icon.png';
 import Button from '../components/Button';
@@ -7,10 +7,10 @@ import InfoBox from '../components/InfoBox';
 
 function Home() {
   return (
-    <main className='home'>
-      <div className='home-container'>
-        <div className='filter'>
-          <form>
+    <main className={styles.home}>
+      <div className={styles.home_container}>
+        <div className={styles.filter}>
+          <form className={styles.form}>
             <input type='text' placeholder='Miestas' value='Miestas' />
             <input type='text' placeholder='Grupė' value='Grupė' />
             <input
@@ -22,27 +22,24 @@ function Home() {
             <button type='submit'>Noriu mokytis!</button>
           </form>
         </div>
-        <div className='hero'>
-          <div className='hero-text'>
-            <h1>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry.
-            </h1>
+        <div className={styles.hero}>
+          <div className={styles.hero_text}>
+            <h1>Tavo karjeros ir tobulėjimo galimybės Lietuvoje!</h1>
             <p>
               Lorem Ipsum has been the industry's standard dummy text ever since
               the 1500s, when an unknown printer took a galley of type and
               scrambled it to make a type specimen book. It has survived not
               only five centuries
             </p>
-            <Button title={'Sužinok daugiau!'} href={'#'} />
+            <Button title={'Sužinok daugiau'} href={'#'} />
           </div>
-          <div className='hero-img'>
-            <img src={heroImg} alt='hero-image' className='hero-image' />
+          <div className={styles.hero_img}>
+            <img src={heroImg} alt='hero-image' className={styles.hero_image} />
           </div>
         </div>
-        <div className='additional-info'>
+        <div className={styles.additional_info}>
           <h2>Kažkokie faktai apie mokslą Lietuvoje</h2>
-          <div className='info-box-container'>
+          <div className={styles.info_box_container}>
             <InfoBox
               text={
                 'Standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries'
