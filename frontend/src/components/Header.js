7dbfae098da9765817ed1c149adf1d8293cbd700
Agne-stat <71026638+Agne-stat@users.noreply.github.com>
2021-04-22 19:30:11 +0300

    login css

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -24,7 +24,7 @@ function Header() {
           <span></span>
         </div>
         <nav className={styles.navbar}>
-          <ul className={styles.navbar__menu_items} ref={navigation}>
+          <ul className={styles.navbar_menu} ref={navigation}>
             <li>
               <Link to='/about'>Apie mus</Link>
             </li>
@@ -35,7 +35,7 @@ function Header() {
               <Link to='/carrer'>Karjera</Link>
             </li>
             <li>
-                <Link to='/admin'>Prisijungti</Link>
+                <Link to='v1/admin/login'>Prisijungti</Link>
             </li>
           </ul>
         </nav>
@@ -45,3 +45,55 @@ function Header() {
 }
 
 export default Header;
+
+
+
+// import React, { useRef} from 'react';
+// import { Link } from 'react-router-dom';
+// import './Header.css';
+// // import styles from './Header.module.css';
+// import logo from '../assets/images/logo.png'
+
+// function Header() {
+//   const navigation = useRef();
+
+
+//   function clickHandler() {
+//     navigation.current.classList.toggle('active');
+//   }
+
+//   return (
+//     <header>
+//       <div className='container'>
+//         <div className='logo'>
+//           <Link to='/v1'>
+//              <img src={logo} alt='logo'></img>
+//           </Link>
+//         </div>
+//         <div className='toggler' onClick={clickHandler}>
+//           <span></span>
+//           <span></span>
+//           <span></span>
+//         </div>
+//         <nav className='navbar'>
+//           <ul className='navbar__menu-items' ref={navigation}>
+//             <li>
+//                <Link to='/about'>Apie mus</Link>
+//              </li>
+//              <li>
+//                <Link to='/faq'>D.U.K</Link>
+//              </li>
+//              <li>
+//                <Link to='/carrer'>Karjera</Link>
+//              </li>
+//              <li>
+//                 <Link to='/admin'>Prisijungti</Link>
+//              </li>
+//           </ul>
+//         </nav>
+//       </div>
+//     </header>
+//   );
+// }
+
+// export default Header;
