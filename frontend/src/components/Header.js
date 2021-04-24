352224d6749c9d16e20d55fc501da54b8e77150d
Agne-stat <71026638+Agne-stat@users.noreply.github.com>
2021-04-24 16:17:22 +0300

    header debug

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,45 +1,44 @@
-import React, { useRef } from 'react';
+import React, { useRef} from 'react';
 import { Link } from 'react-router-dom';
-import styles from './Header.module.css';
-import logo from '../assets/images/logo.png';
+import './Header.css';
+import logo from '../assets/images/logo.png'
 
 function Header(props) {
   const navigation = useRef();
+
 
   function clickHandler() {
     navigation.current.classList.toggle('active');
   }
 
-  console.log(props.url);
-
   return props.url === '/v1/admin/login' ||
-    props.url === '/v1/admin/panel'? null : (
-    <header className={styles.container}>
-      <div className={styles.container}>
-        <div className={styles.logo}>
+  props.url === '/v1/admin/panel'? null : (
+    <header>
+      <div className='container'>
+        <div className='logo'>
           <Link to='/v1'>
-            <img src={logo} alt='logo'></img>
+             <img src={logo} alt='logo'></img>
           </Link>
         </div>
-        <div className={styles.toggler} onClick={clickHandler}>
+        <div className='toggler' onClick={clickHandler}>
           <span></span>
           <span></span>
           <span></span>
         </div>
-        <nav className={styles.navbar}>
-          <ul className={styles.navbar_menu} ref={navigation}>
+        <nav className='navbar'>
+          <ul className='navbar_menu' ref={navigation}>
             <li>
-              <Link to='/about'>Apie mus</Link>
-            </li>
-            <li>
-              <Link to='/faq'>D.U.K</Link>
-            </li>
-            <li>
-              <Link to='/carrer'>Karjera</Link>
-            </li>
-            <li>
+               <Link to='/about'>Apie mus</Link>
+             </li>
+             <li>
+               <Link to='/faq'>D.U.K</Link>
+             </li>
+             <li>
+               <Link to='/carrer'>Karjera</Link>
+             </li>
+             <li>
                 <Link to='v1/admin/login'>Prisijungti</Link>
-            </li>
+             </li>
           </ul>
         </nav>
       </div>
@@ -48,55 +47,3 @@ function Header(props) {
 }
 
 export default Header;
-
-
-
-// import React, { useRef} from 'react';
-// import { Link } from 'react-router-dom';
-// import './Header.css';
-// // import styles from './Header.module.css';
-// import logo from '../assets/images/logo.png'
-
-// function Header() {
-//   const navigation = useRef();
-
-
-//   function clickHandler() {
-//     navigation.current.classList.toggle('active');
-//   }
-
-//   return (
-//     <header>
-//       <div className='container'>
-//         <div className='logo'>
-//           <Link to='/v1'>
-//              <img src={logo} alt='logo'></img>
-//           </Link>
-//         </div>
-//         <div className='toggler' onClick={clickHandler}>
-//           <span></span>
-//           <span></span>
-//           <span></span>
-//         </div>
-//         <nav className='navbar'>
-//           <ul className='navbar__menu-items' ref={navigation}>
-//             <li>
-//                <Link to='/about'>Apie mus</Link>
-//              </li>
-//              <li>
-//                <Link to='/faq'>D.U.K</Link>
-//              </li>
-//              <li>
-//                <Link to='/carrer'>Karjera</Link>
-//              </li>
-//              <li>
-//                 <Link to='/admin'>Prisijungti</Link>
-//              </li>
-//           </ul>
-//         </nav>
-//       </div>
-//     </header>
-//   );
-// }
-
-// export default Header;
